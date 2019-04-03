e0525feb0e419d9aad901f5246fe64c1e1d1ee2c
mjredorici <mtj.redorici@gmail.com>
2019-04-03 22:20:47 +0200

    Try

--- src/WebAPI/Autori/AutoComplete/index.php
+++ src/WebAPI/Autori/AutoComplete/index.php
@@ -1,31 +1,19 @@
-/*[
-  {
-    "Value": "1",
-    "Text": "Giovanni Verga"
-  },
-  {
-    "Value": "2",
-    "Text": "Alessandro Manzoni"
-  },
-  {
-    "Value": "3",
-    "Text": "Italo Svevo"
-  }
-]
-*/
+
 <?php
 
 include 'connection.php';
-$partName= "manz";
+$partName= "Manz";
 
 
 
-    $query = "SELECT Id, Nome, Cognome FROM Autori WHERE Cognome + ' ' + Nome LIKE :part LIMIT 50";
-    $stmt = $db->prepare($query);
+    $query = "SELECT Id AS value, CONCAT(Cognome,' ',Nome) AS text FROM Autori WHERE CONCAT(Cognome,' ',Nome) LIKE '%".":part"."%' LIMIT 50";
+    $stmt = $conn->prepare($query);
+
     $stmt->bindParam(':part', $partName, PDO::PARAM_STR);
+
     $stmt->execute();
     $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    echo json_encode($element);
 
 
 ?>
