9c087fb38b9ec4bb5e59138a0771bf7be03d2bfa
Ivan0011 <amblaivan@gmail.com>
2019-04-05 10:55:06 +0200

    autocomplete completato inizio post ricerca

--- src/UI/js/autoComplete.js
+++ src/UI/js/autoComplete.js
@@ -5,27 +5,29 @@ $(document).ready(
             function(){
                 var val = $("#iAutore").val();
                 console.clear();
-                console.log(val);
                     $.ajax(
                         {
                             type:"GET",
                             url:"../../../mockup/Autori/AutoComplete/?text=" + encodeURI(val),
                             dataType:"json",
                             success: function(data){
-                              console.log(data);
                                 var opzioni = new Array();
                                 for(var i in data){
                                     var opzione = {};
-                                    opzione.value = data[i].value;
                                     opzione.label = data[i].text;
+                                    opzione.value = data[i].value;
                                     opzioni.push(opzione);
-                                    console.log(opzioni);
                                 }
                                 $('#iAutore').autocomplete({
                                     source: opzioni,
                                     select: function (event, ui) {
-                                        $("#iAutore").val(ui.item.value); // display the selected text
-                                        $("#iAutoreHidden").val(ui.item.label); // save selected id to hidden input
+                                        $("#iAutore").val(ui.item.label);
+                                        $("#iAutoreHidden").val(ui.item.value);
+                                        return false;
+                                    },
+                                    focus: function(event, ui) {
+                                      event.preventDefault();
+                                      $("#iAutore").val(ui.item.label);
                                     }
                                 });
 
@@ -34,9 +36,85 @@ $(document).ready(
                                 alert(jqXHR +" "+ textStatus +" "+ errorThrown);
                             }
                         }
-
                     );
+            }
+        );
 
+        $("#iGenere").keyup(
+            function(){
+                var val = $("#iGenere").val();
+                console.clear();
+                    $.ajax(
+                        {
+                            type:"GET",
+                            url:"../../../mockup/Generi/AutoComplete/?text=" + encodeURI(val),
+                            dataType:"json",
+                            success: function(data){
+                                var opzioni = new Array();
+                                for(var i in data){
+                                    var opzione = {};
+                                    opzione.label = data[i].text;
+                                    opzione.value = data[i].value;
+                                    opzioni.push(opzione);
+                                }
+                                $('#iGenere').autocomplete({
+                                    source: opzioni,
+                                    select: function (event, ui) {
+                                        $("#iGenere").val(ui.item.label);
+                                        $("#iGenereHidden").val(ui.item.value);
+                                        return false;
+                                    },
+                                    focus: function(event, ui) {
+                                      event.preventDefault();
+                                      $("#iGenere").val(ui.item.label);
+                                    }
+                                });
+
+                            },
+                            error: function(jqXHR, textStatus, errorThrown){
+                                alert(jqXHR +" "+ textStatus +" "+ errorThrown);
+                            }
+                        }
+                    );
+            }
+        );
+
+        $("#iCasaEditrice").keyup(
+            function(){
+                var val = $("#iCasaEditrice").val();
+                console.clear();
+                    $.ajax(
+                        {
+                            type:"GET",
+                            url:"../../../mockup/CaseEditrici/AutoComplete/?text=" + encodeURI(val),
+                            dataType:"json",
+                            success: function(data){
+                                var opzioni = new Array();
+                                for(var i in data){
+                                    var opzione = {};
+                                    opzione.label = data[i].text;
+                                    opzione.value = data[i].value;
+                                    opzioni.push(opzione);
+                                }
+                                $('#iCasaEditrice').autocomplete({
+                                    source: opzioni,
+                                    select: function (event, ui) {
+                                        $("#iCasaEditrice").val(ui.item.label);
+                                        $("#iCasaEditriceHidden").val(ui.item.value);
+                                        return false;
+                                    },
+                                    focus: function(event, ui) {
+                                      event.preventDefault();
+                                      $("#iCasaEditrice").val(ui.item.label);
+                                    }
+                                });
+
+                            },
+                            error: function(jqXHR, textStatus, errorThrown){
+                                alert(jqXHR +" "+ textStatus +" "+ errorThrown);
+                            }
+                        }
+                    );
             }
         );
 
