42a1b3f93098282d8036d50094f71509a3f1580e
BuildTools <unconfigured@null.spigotmc.org>
2019-04-06 14:04:31 +0200

    Mokups

--- mockup/CaseEditrici/AutoComplete/index.php
+++ mockup/CaseEditrici/AutoComplete/index.php
@@ -1,14 +1,10 @@
-[
-  {
-    "value": "1",
-    "text": "Mondadori"
-  },
-  {
-    "value": "2",
-    "text": "Zanichelli"
-  },
-  {
-    "value": "3",
-    "text": "Disney"
-  }
-]
+[{"value":625,"text":"Pacocha-McCullough"},
+{"value":772,"text":"Schiller, Mitchell and Goyette"},
+{"value":2,"text":"Toy LLC"},
+{"value":338,"text":"Lynch, Glover and Sipes"},
+{"value":357,"text":"Nolan-Russel"},
+{"value":260,"text":"Hegmann-Cummings"},
+{"value":16,"text":"Hoeger-Harris"},
+{"value":506,"text":"Quitzon, Marvin and Little"},
+{"value":208,"text":"Graham, Reinger and Howell"},
+{"value":376,"text":"Zulauf-Stehr"}]
