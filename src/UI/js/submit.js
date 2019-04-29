2bc1651c57e5cea589f0defd77294d6a09432965
BuildTools <unconfigured@null.spigotmc.org>
2019-04-29 20:58:40 +0200

    ricerca completa tranne per i libri e non ci sono le modifiche al volo

--- src/UI/js/submit.js
+++ src/UI/js/submit.js
@@ -15,6 +15,18 @@ $(document).ready(
         libro.CasaEditrice = $("#LibroCasaEditriceHidden").val();
         libro.Scaffale = $("#LibroScaffale").val();
         libro.Armadio = $("#LibroArmadio").val();
+        $.ajax({
+          type: "GET",
+          url: "../../../mockup/Libri/Ricerca",
+          data: {libro:libro},
+          dataType: "json",
+          success: function(data){
+            $("body").html("<table></table>");
+            $.each(data,function(index,element){
+              $("table").append("<tr>"+"<td>" + element.Id + "</td>"+"<td>" + element.Nome + "</td>"+"<td>" + element.LuogoSede + "</td>"+"</tr>");
+            });
+          }
+        });
       }
     );
 
@@ -28,6 +40,18 @@ $(document).ready(
         autore.NascitaA = $("#AutoreNascitaA").val();
         autore.MorteDa = $("#AutoreMorteDa").val();
         autore.MorteA = $("#AutoreMorteA").val();
+        $.ajax({
+          type: "GET",
+          url: "../../../mockup/Autori/Ricerca",
+          data: {autore:autore},
+          dataType: "json",
+          success: function(data){
+            $("body").html("<table></table>");
+            $.each(data,function(index,element){
+              $("table").append("<tr>"+"<td>" + element.Id + "</td>"+"<td>" + element.Nome + "</td>"+"<td>" + element.Cognome + "</td>"+"<td>" + element.DataDiNascita + "</td>"+"<td>" + element.DataDiMorte + "</td>"+"</tr>");
+            });
+          }
+        });
       }
     );
 
@@ -35,6 +59,18 @@ $(document).ready(
       function(){
         var genere = {};
         genere.Id = $("#GenereId").val();
+        $.ajax({
+          type: "GET",
+          url: "../../../mockup/Generi/Ricerca",
+          data: {genere:genere},
+          dataType: "json",
+          success: function(data){
+            $("body").html("<table></table>");
+            $.each(data,function(index,element){
+              $("table").append("<tr>"+"<td>" + element.Id + "</td>"+"</tr>");
+            });
+          }
+        });
       }
     );
 
@@ -45,17 +81,18 @@ $(document).ready(
         casa.Nome = $("#CasaNome").val();
         casa.Luogo = $("#CasaLuogo").val();
         $.ajax({
-          type : "GET",
-          url : "../../../mockup/CaseEditrici/Ricerca/index.php",
-          data : {casa:casa},
-          dataType : "JSON",
-          success : function(data){
-            console.log(data);
-          },
-          error : function(a , b , c){
-            console.log(a + "-" + b + "-" + c);
+          type: "GET",
+          url: "../../../mockup/CaseEditrici/Ricerca",
+          data: {casa:casa},
+          dataType: "json",
+          success: function(data){
+            $("body").html("<table></table>");
+            $.each(data,function(index,element){
+              $("table").append("<tr>"+"<td>" + element.Id + "</td>"+"<td>" + element.Nome + "</td>"+"<td>" + element.LuogoSede + "</td>"+"</tr>");
+            });
           }
         });
+
       }
     );
 
