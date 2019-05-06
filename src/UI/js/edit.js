762c1d2049287599be643d640807953b2cd33dfb
BuildTools <unconfigured@null.spigotmc.org>
2019-05-06 19:32:07 +0200

    edit.js ajax requests to be completed: generi and libri

--- src/UI/js/edit.js
+++ src/UI/js/edit.js
@@ -1,6 +1,75 @@
 $(document).ready(
   function() {
     var id = window.location.search.substr(1);
-    alert(id);
+    if (id != null && id != "") {
+      var pagina = $("body").attr("page");
+
+      switch (pagina) {
+        case "autori":
+        $("button").html("Modifica Autore ID: " + id);
+        $.ajax({
+          type: "GET",
+          url: "../../../mockup/Autori/Modifica",
+          data: {id:id},
+          dataType: "json",
+          success: function(data){
+            $("#AutoreNome").val(data.Nome);
+            $("#AutoreCognome").val(data.Cognome);
+            $("#AutoreNascita").val(data.DataDiNascita);
+            $("#AutoreMorte").val(data.DataDiMorte);
+          }
+        });
+        break;
+
+        case "case":
+        $("button").html("Modifica Casa ID: " + id);
+        $.ajax({
+          type: "GET",
+          url: "../../../mockup/CaseEditrici/Modifica",
+          data: {id:id},
+          dataType: "json",
+          success: function(data){
+            $("#CasaNome").val(data.Nome);
+            $("#CasaLuogo").val(data.LuogoSede);
+          }
+        });
+        break;
+
+        case "generi":
+        $("button").html("Modifica Genere ID: " + id);
+        $.ajax({
+          type: "GET",
+          url: "../../../mockup/Generi/Modifica",
+          data: {id:id},
+          dataType: "json",
+          success: function(data){
+            $("#AutoreNome").val(data.Nome);
+            $("#AutoreCognome").val(data.Cognome);
+            $("#AutoreNascita").val(data.DataDiNascita);
+            $("#AutoreMorte").val(data.DataDiMorte);
+          }
+        });
+        break;
+
+        case "libri":
+        $("button").html("Modifica Libro ID: " + id);
+        $.ajax({
+          type: "GET",
+          url: "../../../mockup/Libri/Modifica",
+          data: {id:id},
+          dataType: "json",
+          success: function(data){
+            $("#AutoreNome").val(data.Nome);
+            $("#AutoreCognome").val(data.Cognome);
+            $("#AutoreNascita").val(data.DataDiNascita);
+            $("#AutoreMorte").val(data.DataDiMorte);
+          }
+        });
+        break;
+
+        default:
+        break;
+      }
+    }
   }
 );
