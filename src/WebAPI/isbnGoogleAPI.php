39c8b06d080dc6d2587186e0313292288f45d969
Ivan0011 <amblaivan@gmail.com>
2019-05-10 10:30:03 +0200

    api

--- src/WebAPI/isbnGoogleAPI.php
+++ src/WebAPI/isbnGoogleAPI.php
@@ -1,19 +1,18 @@
 <?php
 
-$isbn= "9788804671657";//file_get_contents('php://input');//'9788804671657';
+$isbn= file_get_contents('php://input');
 $url = "https://www.googleapis.com/books/v1/volumes?q=isbn:".$isbn;
 // to check your proxy
-$proxy = '192.168.152.1:808';
+$proxy = '192.168.153.1:808';
 
 // create curl resource
 $ch = curl_init();
 
 // set options
 curl_setopt($ch, CURLOPT_URL, $url);
-//curl_setopt($ch, CURLOPT_PROXY, $proxy); // da mettere a commento se non si utilizza il proxy
+curl_setopt($ch, CURLOPT_PROXY, $proxy); // da mettere a commento se non si utilizza il proxy
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
 // $output contains the output string
 $output = curl_exec($ch);
@@ -21,27 +20,7 @@ $output = curl_exec($ch);
 // close curl resource to free up system resources
 curl_close($ch);
 $json = json_decode($output,true);
-
-//creazione variabili informazioni libro
-$titolo = $json['items']['0']['volumeInfo']['title'];
-$genere = $json['items']['0']['volumeInfo']['subtitle'];
-$autore = $json['items']['0']['volumeInfo']['authors'][0];
-$casaEditrice = $json['items']['0']['volumeInfo']['publisher'];
-$dataPubblicazione = $json['items']['0']['volumeInfo']['publishedDate'];
-$descrizione = $json['items']['0']['volumeInfo']['description'];
-$pagine = $json['items']['0']['volumeInfo']['pageCount'];
-
-//array con tutte le informazioni del libro
-$libro = [
-    'titolo' => "$titolo",
-    'genere' => "$genere",
-    'autore' => "$autore",
-    'CasaEditrice' => "$casaEditrice",
-    'dataPubblicazione' => "$dataPubblicazione",
-    'Descrizione' => "$descrizione",
-    'Pagine' => "$pagine"
-];
+$libro = $json['items']['0']['volumeInfo'];
 echo json_encode($libro);
-
 
 ?>
